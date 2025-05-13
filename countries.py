2ad186eff85e6ed53d059ca29f7f53653783a509
Simon Vreman <info@simonvreman.nl>
2025-05-13 10:02:47 +0200

    Cleanup

---
+++ countries.py
@@ -0,0 +1,99 @@
+import geopandas as gpd
+from main import (
+    open_utf8,
+    read_boundaries,
+    write_paths,
+    lat_lon_to_mercator,
+    normalize_string,
+)
+
+rounding = 3
+
+countries_used = [
+    "France",
+    "Belgium",
+    "Germany",
+    "Italy",
+    "Spain",
+    "Portugal",
+    "Netherlands",
+    "Luxembourg",
+    "Ireland",
+    "United Kingdom",
+    "Switzerland",
+    "Austria",
+    "Denmark",
+    "Finland",
+    "Norway",
+    "Sweden",
+    "Iceland",
+    "Czechia",
+    "Slovakia",
+    "Hungary",
+    "Poland",
+    "Romania",
+    "Bulgaria",
+    "Croatia",
+    "Republic of Serbia",
+    "Bosnia and Herzegovina",
+    "Montenegro",
+    "North Macedonia",
+    "Albania",
+    "Greece",
+    "Turkey",
+    "Cyprus",
+    "Malta",
+    "Monaco",
+    "San Marino",
+    "Andorra",
+    "Liechtenstein",
+    "Slovenia",
+    "Estonia",
+    "Latvia",
+    "Lithuania",
+    "Belarus",
+    "Ukraine",
+    "Russia",
+    "Kosovo",
+    "Moldova",
+]
+
+
+def write_countries():
+    df = gpd.read_file("input/natural_earth_countries/ne_10m_admin_0_countries_nld.shp")
+
+    for country in countries_used:
+        country_name_parts = normalize_string(country).split(" ")
+        filename = "-".join(country_name_parts)
+        constant_name = country_name_parts[0] + "".join(
+            x.capitalize() for x in country_name_parts[1:]
+        )
+
+        df_country = df[df["ADMIN"] == country]
+        if df_country.empty:
+            print(f"Warning: No data found for {country}")
+            continue
+
+        boundaries, _ = read_boundaries(df_country, union=True)
+        output = open_utf8(f"output/{filename}.tsx", "w")
+        output.write(
+            f"export const {constant_name}Paths = {'<>' if len(boundaries) > 1 else '('}\n"
+        )
+
+        write_paths(output, boundaries)
+
+        output.write(f"{'</>' if len(boundaries) > 1 else ')'}\n")
+
+        bounds = df_country.total_bounds
+        center = df_country.geometry.union_all().representative_point()
+        west, north = lat_lon_to_mercator(bounds[3], bounds[0])
+        east, south = lat_lon_to_mercator(bounds[1], bounds[2])
+        center_x, center_y = lat_lon_to_mercator(center.y, center.x)
+        output.write(
+            f"export const {constant_name}Meta = {{ west: {round(west, rounding)}, north: {round(north, rounding)}, east: {round(east, rounding)}, south: {round(south, rounding)}, x: {round(center_x, rounding)}, y: {round(center_y, rounding)}  }}\n"
+        )
+
+        output.close()
+
+
+# write_countries()
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import geopandas as gpd
-import math
-import shapely
-from brazil_ddd import ddds
-from spain import spain_area_codes
 import unicodedata
+from functools import partial
+import math
+import geopandas as gpd
+import shapely
+
+open_utf8 = partial(open, encoding="UTF-8")
 
 mercator_reach = 85.05112877980659
 x_domain = 1000
 rounding = 3
-simplify = 0.025
+simplify = 0.01
+
+
+class BoundaryPaths:
+    def __init__(self, exterior: list[float], interiors: list[list[float]] = None):
+        self.exterior = exterior
+        self.interiors = interiors
 
 
 def lat_lon_to_mercator(lat, lon):
@@ -28,7 +35,9 @@ def lat_lon_to_mercator(lat, lon):
     return (x, y)
 
 
-def read_boundaries(df: gpd.GeoDataFrame, union=False):
+def read_boundaries(
+    df: gpd.GeoDataFrame, union=False
+) -> tuple[list[BoundaryPaths], tuple[float, float]]:
     # Explode the multipolygon into individual polygons
     df = df.explode()
 
@@ -38,35 +47,94 @@ def read_boundaries(df: gpd.GeoDataFrame, union=False):
     union_geometry = df.geometry.union_all()
 
     # Convert the geometries to a list of simplified paths
-    paths = []
+    boundaries: list[BoundaryPaths] = []
     geometries = [union_geometry] if union else df.geometry.to_list()
-    for polygon in geometries:
-        if isinstance(polygon, shapely.geometry.polygon.Polygon):
-            paths.append(list(polygon.simplify(simplify).exterior.coords))
-        elif isinstance(polygon, shapely.geometry.multipolygon.MultiPolygon):
-            for poly in polygon.geoms:
-                paths.append(list(poly.simplify(simplify).exterior.coords))
+
+    for shape in geometries:
+        polygons = (
+            [shape]
+            if isinstance(shape, shapely.Polygon)
+            else shape.geoms if isinstance(shape, shapely.MultiPolygon) else []
+        )
+
+        for polygon in polygons:
+            simplified = polygon.simplify(simplify)
+            if not isinstance(simplified, shapely.Polygon):
+                continue
+            boundaries.append(
+                BoundaryPaths(
+                    list(simplified.exterior.coords),
+                    [i.coords for i in list(simplified.interiors)],
+                )
+            )
 
     representative = union_geometry.representative_point()
 
-    return paths, lat_lon_to_mercator(representative.y, representative.x)
+    return boundaries, lat_lon_to_mercator(representative.y, representative.x)
 
 
-def write_paths(output, paths):
-    for path in paths:
-        output.write('<path d="M')
-        is_first = True
+def unioned_or_largest(geometry):
+    unioned = shapely.unary_union(geometry)
 
-        for lon, lat in path:
-            if is_first:
-                is_first = False
-            else:
-                output.write("L")
+    if isinstance(unioned, shapely.geometry.polygon.Polygon):
+        return unioned
 
-            x, y = lat_lon_to_mercator(lat, lon)
-            output.write(f" {round(x, rounding)} {round(y, rounding)} ")
+    return max(unioned.geoms, key=lambda polygon: polygon.area)
 
-        output.write('z" />\n')
+
+def read_internal_boundaries(df: gpd.GeoDataFrame) -> list[BoundaryPaths]:
+    indexed = df.reset_index(drop=False)
+    indexed["geometry"] = indexed.apply(
+        lambda row: unioned_or_largest(row.geometry),
+        axis=1,
+    )
+    crossed = indexed.merge(indexed, how="cross")
+    crossed = crossed[(crossed["index_x"] > crossed["index_y"])]
+
+    crossed["intersection"] = crossed.apply(
+        lambda row: shapely.line_merge(
+            shapely.intersection(row.geometry_x, row.geometry_y, grid_size=0.01),
+        ),
+        axis=1,
+    )
+
+    paths = []
+    for line in crossed["intersection"].to_list():
+        lines = [line] if isinstance(line, shapely.LineString) else line.geoms
+        paths.extend(
+            [BoundaryPaths(list(line.simplify(simplify).coords)) for line in lines]
+        )
+
+    return paths
+
+
+def path_to_string(path: list[float]) -> str:
+    result = "M"
+    is_first = True
+
+    for lon, lat in path:
+        if is_first:
+            is_first = False
+        else:
+            result += " L"
+
+        x, y = lat_lon_to_mercator(lat, lon)
+        result += f" {round(x, rounding)} {round(y, rounding)}"
+
+    return result
+
+
+def write_paths(output, boundaries: list[BoundaryPaths], close=True, interiors=False):
+    for boundary in boundaries:
+        output.write(
+            f'<path d="{path_to_string(boundary.exterior)}{' Z' if close else ''}'
+        )
+
+        if interiors and boundary.interiors:
+            for interior in boundary.interiors:
+                output.write(f" {path_to_string(interior)} Z")
+
+        output.write('" />\n')
 
 
 def normalize_string(s):
@@ -76,187 +144,3 @@ def normalize_string(s):
     )
     s = s.replace("’", "'").replace("´", "'").replace("`", "'")
     return s
-
-
-def write_brazil():
-    df = gpd.read_file("input/brazil/bra_admbnda_adm0_ibge_2020.shp")
-    output = open("output/brazil.path.txt", "w")
-    paths, _ = read_boundaries(df)
-    write_paths(output, paths)
-    output.close()
-
-
-def write_brazil_first_administrative():
-    df = gpd.read_file("input/brazil/bra_admbnda_adm1_ibge_2020.shp")
-    output = open("output/brazil-first-administrative.path.txt", "w")
-    paths, _ = read_boundaries(df)
-    write_paths(output, paths)
-    output.close()
-
-
-def write_brazil_phone():
-    df = gpd.read_file("input/brazil/bra_admbnda_adm2_ibge_2020.shp")
-    df["ADM2_PT_normalized"] = df["ADM2_PT"].apply(normalize_string)
-
-    output = open("output/brazil-dialing-codes.path.txt", "w")
-    output.write("export const brazilPhoneCodes = [\n")
-
-    for ddd in ddds.keys():
-        normalized_ddd_cities = set(map(normalize_string, ddds[ddd]["adm2"]))
-        normalized_ddd_2_cities = (
-            set(map(normalize_string, ddds[ddd]["adm2_2"]))
-            if "adm1_2" in ddds[ddd]
-            else set()
-        )
-
-        df_ddd = (
-            df[
-                (df["ADM1_PT"] == ddds[ddd]["adm1"])
-                & df["ADM2_PT_normalized"].isin(normalized_ddd_cities)
-            ]
-            if "adm1_2" not in ddds[ddd]
-            else df[
-                (
-                    (df["ADM1_PT"] == ddds[ddd]["adm1"])
-                    & df["ADM2_PT_normalized"].isin(normalized_ddd_cities)
-                )
-                | (
-                    (df["ADM1_PT"] == ddds[ddd]["adm1_2"])
-                    & df["ADM2_PT_normalized"].isin(normalized_ddd_2_cities)
-                )
-            ]
-        )
-
-        cities_not_in_df = (normalized_ddd_cities | normalized_ddd_2_cities) - set(
-            df_ddd["ADM2_PT_normalized"]
-        )
-        if len(cities_not_in_df) > 0:
-            print(
-                f"Warning: Not all cities found for DDD {ddd} in {ddds[ddd]['adm1']}, missing: {cities_not_in_df}"
-            )
-
-        paths, center = read_boundaries(df_ddd, union=True)
-        output.write(
-            f"{{ code: {ddd}, center: [{round(center[0], rounding)},{round(center[1], rounding)}], paths: ({'<>' if len(paths) > 1 else ''}"
-        )
-        write_paths(output, paths)
-        output.write(f"{'</>' if len(paths) > 1 else ''})}},\n")
-
-    output.write("]\n")
-    output.close()
-
-
-def write_spain_phone():
-    df = gpd.read_file("input/spain/whosonfirst-data-admin-es-region-polygon.shp")
-
-    output = open("output/spain-dialing-codes.path.txt", "w")
-    output.write("export const spainPhoneCodes = [\n")
-
-    for code in spain_area_codes.keys():
-        df_ddd = df[df["name_eng"].isin(spain_area_codes[code]["adm2"])]
-        if (
-            "Santa Cruz de Tenerife" in spain_area_codes[code]["adm2"]
-            or "Las Palmas" in spain_area_codes[code]["adm2"]
-        ):
-            df_ddd = df_ddd.translate(xoff=5, yoff=9)
-
-        paths, center = read_boundaries(df_ddd, union=True)
-        output.write(
-            f"{{ code: {code}, center: [{round(center[0], rounding)},{round(center[1], rounding)}], paths: ({'<>' if len(paths) > 1 else ''}"
-        )
-        write_paths(output, paths)
-        output.write(f"{'</>' if len(paths) > 1 else ''})}},\n")
-
-    output.write("]\n")
-    output.close()
-
-
-def write_european_countries():
-    df = gpd.read_file("input/natural_earth_countries/ne_10m_admin_0_countries_nld.shp")
-
-    for country in [
-        "France",
-        "Belgium",
-        "Germany",
-        "Italy",
-        "Spain",
-        "Portugal",
-        "Netherlands",
-        "Luxembourg",
-        "Ireland",
-        "United Kingdom",
-        "Switzerland",
-        "Austria",
-        "Denmark",
-        "Finland",
-        "Norway",
-        "Sweden",
-        "Iceland",
-        "Czechia",
-        "Slovakia",
-        "Hungary",
-        "Poland",
-        "Romania",
-        "Bulgaria",
-        "Croatia",
-        "Republic of Serbia",
-        "Bosnia and Herzegovina",
-        "Montenegro",
-        "North Macedonia",
-        "Albania",
-        "Greece",
-        "Turkey",
-        "Cyprus",
-        "Malta",
-        "Monaco",
-        "San Marino",
-        "Andorra",
-        "Liechtenstein",
-        "Slovenia",
-        "Estonia",
-        "Latvia",
-        "Lithuania",
-        "Belarus",
-        "Ukraine",
-        "Russia",
-        "Kosovo",
-        "Moldova",
-    ]:
-        country_name_parts = normalize_string(country).split(" ")
-        filename = "-".join(country_name_parts)
-        constant_name = country_name_parts[0] + "".join(
-            x.capitalize() for x in country_name_parts[1:]
-        )
-
-        df_country = df[df["ADMIN"] == country]
-        if df_country.empty:
-            print(f"Warning: No data found for {country}")
-            continue
-        paths, _ = read_boundaries(df_country, union=True)
-
-        output = open(f"output/{filename}.tsx", "w")
-        output.write(
-            f"export const {constant_name}Paths = {'<>' if len(paths) > 1 else '('}\n"
-        )
-
-        write_paths(output, paths)
-
-        output.write(f"{'</>' if len(paths) > 1 else ')'}\n")
-
-        bounds = df_country.total_bounds
-        center = df_country.geometry.union_all().representative_point()
-        west, north = lat_lon_to_mercator(bounds[3], bounds[0])
-        east, south = lat_lon_to_mercator(bounds[1], bounds[2])
-        center_x, center_y = lat_lon_to_mercator(center.y, center.x)
-        output.write(
-            f"export const {constant_name}Meta = {{ west: {round(west, rounding)}, north: {round(north, rounding)}, east: {round(east, rounding)}, south: {round(south, rounding)}, x: {round(center_x, rounding)}, y: {round(center_y, rounding)}  }}\n"
-        )
-
-        output.close()
-
-
-# write_brazil_phone()
-# write_brazil()
-write_brazil_first_administrative()
-# write_european_countries()
-# write_spain_phone()
