296881ae9a0fdb2ca206d663918d60e097bd20a3
Simon Vreman <info@simonvreman.nl>
2025-05-27 11:25:47 +0200

    Use offscreen canvas per country

--- components/canvas/game/selectable-patterns.tsx
+++ components/canvas/game/selectable-patterns.tsx
@@ -25,9 +25,11 @@ const renderKeys = {
 } as const;
 
 const patternCache = new Map<string, HTMLCanvasElement>();
-const pathsCache = new WeakMap<Path2D[], Path2D>();
+const entryCache = new Map<string, HTMLCanvasElement>();
+const pathCache = new WeakMap<Path2D[], Path2D>();
 
 const patternKey = (n: string, s: number) => `${n}:${s}`;
+const entryKey = (n: string, s: number) => `${n}:${s}`;
 
 function cachedPattern({
   subject,
@@ -44,6 +46,8 @@ function cachedPattern({
   const key = patternKey(subject, scale);
   if (patternCache.has(key)) return patternCache.get(key)!;
 
+  // console.log(`Rendering pattern: ${subject} at scale: ${scale}`);
+
   const offscreen = document.createElement("canvas");
   offscreen.width = 400 * scale + 20;
   offscreen.height = 500 * scale + 20;
@@ -59,17 +63,94 @@ function cachedPattern({
   return offscreen;
 }
 
-function cachedMergedPath(paths: Path2D[]) {
-  let mergedPath = pathsCache.get(paths);
-  if (mergedPath) return mergedPath;
+function cachedEntry<TMap extends Record<string, Pattern>>({
+  entry: { name, paths, meta, transform: baseTransform, subjects },
+  scale,
+  drawPattern,
+}: {
+  entry: PatternEntry<TMap>;
+  scale: number;
+  drawPattern: (args: {
+    subject: string;
+    ctx: CanvasRenderingContext2D;
+  }) => void;
+}) {
+  const key = entryKey(name, scale);
+  if (entryCache.has(key)) return entryCache.get(key)!;
 
-  mergedPath = paths.reduce((acc, path) => {
-    acc.addPath(path);
-    return acc;
-  }, new Path2D());
+  let mergedPath = pathCache.get(paths);
+  if (!mergedPath) {
+    mergedPath = paths.reduce((acc, path) => {
+      acc.addPath(path);
+      return acc;
+    }, new Path2D());
+    pathCache.set(paths, mergedPath);
+  }
 
-  pathsCache.set(paths, mergedPath);
-  return mergedPath;
+  const offscreen = document.createElement("canvas");
+
+  offscreen.width = Math.ceil((meta.east - meta.west) * scale);
+  offscreen.height = Math.ceil((meta.south - meta.north) * scale);
+
+  // console.log(
+  //   `Rendering entry: ${name} at scale: ${scale}, width: ${offscreen.width}, height: ${offscreen.height}`
+  // );
+
+  const ctx = offscreen.getContext("2d")!;
+
+  ctx.transform(scale, 0, 0, scale, -meta.west * scale, -meta.north * scale);
+  ctx.clip(mergedPath);
+
+  const rasterScale = baseTransform[0] * 2 * scale;
+  const transform: typeof baseTransform = [...baseTransform];
+  transform[0] /= rasterScale;
+  transform[3] /= rasterScale;
+  ctx.transform(...transform);
+
+  const height = 500 * rasterScale;
+  const maxYOffset = (meta.south - meta.north) / transform[3] + height;
+
+  let i = 0;
+
+  for (let yOffset = 0; yOffset < maxYOffset; yOffset += height) {
+    const width = 400 * rasterScale;
+    const maxXOffset = (meta.east - meta.west) / transform[0] + width;
+    const subject = subjects[i % subjects.length] as string;
+
+    for (let xOffset = 0; xOffset < maxXOffset; xOffset += width) {
+      // if (
+      //   xOffset + width < topLeft.x ||
+      //   xOffset > bottomRight.x ||
+      //   yOffset + height < topLeft.y ||
+      //   yOffset > bottomRight.y
+      // )
+      //   continue;
+
+      const cached = cachedPattern({
+        subject,
+        scale: rasterScale,
+        drawPattern,
+      });
+
+      if (cached) ctx.drawImage(cached, xOffset, yOffset);
+    }
+
+    i++;
+  }
+
+  entryCache.set(key, offscreen);
+  // ctx.transform(...transform);
+
+  // const dpr = window.devicePixelRatio;
+  // const rawTopLeft = bounding.current;
+  // const rawBottomRight = {
+  //   x: bounding.current.width * dpr,
+  //   y: bounding.current.height * dpr,
+  // };
+
+  // const tMatrix = ctx.getTransform().inverse();
+  // const topLeft = tMatrix.transformPoint(rawTopLeft);
+  // const bottomRight = tMatrix.transformPoint(rawBottomRight);
 }
 
 export function SelectablePatterns<
@@ -117,86 +198,43 @@ export function SelectablePatterns<
   );
 
   const entryRenderer = useCallback(
-    ({
-        paths,
-        name,
-        subjects,
-        transform: baseTransform,
-        meta,
-      }: TEntry): Renderer =>
+    (entry: TEntry): Renderer =>
       ({ ctx, scale }) => {
         ctx.strokeStyle = twColor("neutral-300", "neutral-700");
         ctx.lineWidth = scale;
         ctx.lineJoin = "round";
 
-        const highlighted = isHighlighted(name);
+        const highlighted = isHighlighted(entry.name);
+
+        if (["Russia", "France"].includes(entry.name)) return;
 
         if (highlighted) {
-          const transform: typeof baseTransform = [...baseTransform];
-          const rasterScale =
-            2 * 2 ** Math.round(Math.log2(baseTransform[0] / scale));
+          const rasterScale = 2 ** Math.round(Math.log2(1 / scale));
 
-          transform[0] /= rasterScale;
-          transform[3] /= rasterScale;
+          ctx.scale(1 / rasterScale, 1 / rasterScale);
 
-          ctx.save();
+          const cached = cachedEntry({
+            entry,
+            scale: rasterScale,
+            drawPattern,
+          });
 
-          const mergedPath = cachedMergedPath(paths);
+          if (cached)
+            ctx.drawImage(
+              cached,
+              entry.meta.west * rasterScale,
+              entry.meta.north * rasterScale
+            );
 
-          ctx.clip(mergedPath);
-          ctx.transform(...transform);
-
-          const dpr = window.devicePixelRatio;
-          const rawTopLeft = bounding.current;
-          const rawBottomRight = {
-            x: bounding.current.width * dpr,
-            y: bounding.current.height * dpr,
-          };
-
-          const tMatrix = ctx.getTransform().inverse();
-          const topLeft = tMatrix.transformPoint(rawTopLeft);
-          const bottomRight = tMatrix.transformPoint(rawBottomRight);
-
-          const height = 500 * rasterScale;
-          const maxYOffset = (meta.south - meta.north) / transform[3] + height;
-
-          let i = 0;
-          const cached: HTMLCanvasElement[] = [];
-
-          for (let yOffset = 0; yOffset < maxYOffset; yOffset += height) {
-            const width = 400 * rasterScale;
-            const maxXOffset = (meta.east - meta.west) / transform[0] + width;
-            const subject = subjects[i] as string;
-
-            for (let xOffset = 0; xOffset < maxXOffset; xOffset += width) {
-              if (
-                xOffset + width < topLeft.x ||
-                xOffset > bottomRight.x ||
-                yOffset + height < topLeft.y ||
-                yOffset > bottomRight.y
-              )
-                continue;
-
-              cached[i] ??= cachedPattern({
-                subject,
-                scale: rasterScale,
-                drawPattern,
-              });
-
-              ctx.drawImage(cached[i], xOffset, yOffset);
-            }
-
-            i = (i + 1) % subjects.length;
-          }
-          ctx.restore();
+          ctx.scale(rasterScale, rasterScale);
         }
 
-        for (const path of paths) {
+        for (const path of entry.paths) {
           ctx.fill(path);
           ctx.stroke(path);
         }
       },
-    [isHighlighted, drawPattern, bounding]
+    [isHighlighted, drawPattern]
   );
 
   useDynamicFill({
